9669e7d01df37e94eab7e960e1403bdc25f6285d
mahajanmuskaan <er.muskaan21@gmail.com>
2023-06-06 18:32:53 +0530

    updated code-for fetching real time data

--- src/component/BodyComponent.js
+++ src/component/BodyComponent.js
@@ -1,17 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Food1 from '../../images/Foodimage1.jpg';
 import Food2 from '../../images/Foodimage2.jpg';
 import Food3 from '../../images/Foodimage3.jpg';
 import Food5 from '../../images/carousel-item2.jpg';
 import Restaurant from './Restaurant'
-import { restaurantList } from '../config.js';
+//import { restaurantList } from '../config.js';
 
 // Body Component
 
 const MainBodyComponent = () => {
     // Declare state variable searchText and its setter function setSearchText
     const [searchText, setSearchText] = useState('');
-    const [restaurants, setRestaurants] = useState(restaurantList);
+
+    //the restaurants state always contains the filtered data, while originalData retains the original data fetched from the API for each new filtering operation.
+    const [restaurants, setRestaurants] = useState([]);
+    const [originalrestaurants, setOriginalRestaurants] = useState([]);
+
     const [noResults, setNoResults] = useState(false); // New state variable
     const [filterBy, setFilterBy] = useState('');
 
@@ -22,28 +26,28 @@ const MainBodyComponent = () => {
     };
 
     //Restaurant Search - Search based on Restaurant Name
-    function filterdata(searchText, restaurantList) {
-        return (restaurantList.filter((restaurant) => {
+    function filterdata(searchText, originalrestaurants) {
+        return (originalrestaurants.filter((restaurant) => {
             return restaurant.data.name.toLowerCase().includes(searchText.toLowerCase());
         }
         ));
     }
     // Restaurant Search
-    function searchData(searchText, restaurantList) {
+    function searchData(searchText, originalrestaurants) {
         if (searchText !== '') {
-            const filteredData = filterdata(searchText, restaurantList);
+            const filteredData = filterdata(searchText, originalrestaurants);
             setRestaurants(filteredData);
             setNoResults(filteredData.length === 0); // Set noResults based on search results. if (filteredData.length === 0)-> true, then no results are there else, results are there.
         }
         else {
-            setRestaurants(restaurantList);
+            setRestaurants(originalrestaurants);
             setNoResults(false); // Reset noResults // Incase no text is given in search box, so all restaurants should be shown by default.
         }
     }
 
     // FilterOption- Search based on cuisines
     function filterOptions(filterBy) {
-        return restaurantList.filter((restaurant) => {
+        return originalrestaurants.filter((restaurant) => {
             return restaurant.data.cuisines.some((cuisine) =>
                 cuisine.toLowerCase().includes(filterBy.toLowerCase())
             );
@@ -57,8 +61,34 @@ const MainBodyComponent = () => {
             setNoResults(filteredOptionList.length === 0);
         } else {
             setFilterBy('');
-            setRestaurants(restaurantList);
+            setRestaurants(originalrestaurants);
             setNoResults(false);
+        }
+    }
+
+    // useEffect Hook Usage to make a API call- to fetch Real data from swiggy API.
+    useEffect(() => {
+        // Fetch Restaurant Data
+        getRestaurants();
+
+    }, []);
+    async function getRestaurants() {
+        // To handle errors
+        const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=31.6314253&lng=74.84340259999999&page_type=DESKTOP_WEB_LISTING", {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+            },
+        });
+        if (response.ok) {
+            const json_data = await response.json();
+            const fetchedAPIData = json_data?.data?.cards[2]?.data?.data?.cards;
+            console.log(fetchedAPIData);
+            setOriginalRestaurants(fetchedAPIData);//original data fetched from the Swiggy API
+            setRestaurants(fetchedAPIData);
+        }
+        else {
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
     }
 
@@ -76,6 +106,7 @@ const MainBodyComponent = () => {
             </div>
             <div className="restaurant-body-section">
                 <h2>Find Restaurants here...</h2>
+                
                 {/* Search Bar */}
                 <div className="search-section">
                     <div className="search-box">
@@ -87,14 +118,14 @@ const MainBodyComponent = () => {
                             onChange={handleChange}
                         />
                         <button className="search-box-btn" onClick={() =>
-                            searchData(searchText, restaurantList)
+                            searchData(searchText, originalrestaurants)
                         }>
                             Search
                         </button>
                     </div>
                 </div>
 
-                {/* Filters and Sort Options */}
+                {/* Filters Options */}
                 <div className="filters_sort">
                     <div className="filters_options">
                         <ul>
@@ -106,10 +137,11 @@ const MainBodyComponent = () => {
                             <li onClick={() => filteredOption('Italian')}>Italian</li>|
                             <li onClick={() => filteredOption('Continental')}>Continental</li>|
                             <li onClick={() => filteredOption('Mughlai')}>Mughlai</li>|
-                            <li onClick={() => filteredOption('Chinese')}>Chinese</li>|
                             <li onClick={() => filteredOption('Afghani')}>Afghani</li>|
+                            <li onClick={() => filteredOption('Snacks')}>Snacks</li>|
                             <li onClick={() => filteredOption('Salads')}>Salads</li>|
                             <li onClick={() => filteredOption('Beverages')}>Beverages</li>|
+                            <li onClick={() => filteredOption('Desserts')}>Desserts</li>|
                             <li onClick={() => filteredOption('Bakery')}>Bakery</li>
                             <li id="reset_all" onClick={() => filteredOption('')}>Reset Filter</li>
                         </ul>
--- src/config.js
+++ src/config.js
@@ -24,7 +24,7 @@ export const restaurantList = [
             area: "Anand Vihar Colony",
             totalRatingsString: "5000+ ratings",
             cloudinaryImageId: "w0fc1sr85flnqzb7qhma",
-            cuisines: ["South Indian", "North Indian","Snacks", "Biryani"],
+            cuisines: ["South Indian", "North Indian", "Snacks", "Biryani"],
             tags: [],
             costForTwo: 30000,
             costForTwoString: "₹300 FOR TWO",
